e6603a1348b917783c98c5c0f23176768d25371c
ben fleis <benfleis@uber.com>
2020-02-06 14:35:49 +0100

    add -, *, / operators and tests

--- src/main/Builtins.kt
+++ src/main/Builtins.kt
@@ -5,7 +5,7 @@ import lispy.Expr.*
 /**
  * builtin procedures, made generally available in base environment.
  */
-fun add2(a: Expr, b: Expr): Expr =
+internal fun add2(a: Expr, b: Expr): Expr =
     when {
         a is LongNumeric && b is LongNumeric -> LongNumeric(a.long + b.long)
         a is LongNumeric && b is DoubleNumeric -> DoubleNumeric(a.long.toDouble() + b.double)
@@ -14,9 +14,72 @@ fun add2(a: Expr, b: Expr): Expr =
         else -> throw IllegalArgumentException("Procedure received non-Numeric argument")
     }
 
+internal fun subtract2(a: Expr, b: Expr): Expr =
+    when {
+        a is LongNumeric && b is LongNumeric -> LongNumeric(a.long - b.long)
+        a is LongNumeric && b is DoubleNumeric -> DoubleNumeric(a.long.toDouble() - b.double)
+        a is DoubleNumeric && b is LongNumeric -> DoubleNumeric(a.double - b.long.toDouble())
+        a is DoubleNumeric && b is DoubleNumeric -> DoubleNumeric(a.double - b.double)
+        else -> throw IllegalArgumentException("Procedure received non-Numeric argument")
+    }
+
+internal fun multiply2(a: Expr, b: Expr): Expr =
+    when {
+        a is LongNumeric && b is LongNumeric -> LongNumeric(a.long * b.long)
+        a is LongNumeric && b is DoubleNumeric -> DoubleNumeric(a.long.toDouble() * b.double)
+        a is DoubleNumeric && b is LongNumeric -> DoubleNumeric(a.double * b.long.toDouble())
+        a is DoubleNumeric && b is DoubleNumeric -> DoubleNumeric(a.double * b.double)
+        else -> throw IllegalArgumentException("Procedure received non-Numeric argument")
+    }
+
+internal fun divide2(a: Expr, b: Expr): Expr =
+    when {
+        a is LongNumeric && b is LongNumeric -> LongNumeric(a.long / b.long)
+        a is LongNumeric && b is DoubleNumeric -> DoubleNumeric(a.long.toDouble() / b.double)
+        a is DoubleNumeric && b is LongNumeric -> DoubleNumeric(a.double / b.long.toDouble())
+        a is DoubleNumeric && b is DoubleNumeric -> DoubleNumeric(a.double / b.double)
+        else -> throw IllegalArgumentException("Procedure received non-Numeric argument")
+    }
+
+internal inline fun negate(expr: Expr): Expr =
+    when (expr) {
+        is LongNumeric -> LongNumeric(-expr.long)
+        is DoubleNumeric -> DoubleNumeric(-expr.double)
+        else -> throw IllegalArgumentException("Procedure received non-Numeric argument")
+    }
+
+internal inline fun invert(expr: Expr): Expr =
+    when (expr) {
+        is LongNumeric -> LongNumeric(1 / expr.long)
+        is DoubleNumeric -> DoubleNumeric(1.0 / expr.double)
+        else -> throw IllegalArgumentException("Procedure received non-Numeric argument")
+    }
+
 fun add(vararg args: Expr): Expr = args.asSequence().fold(LongNumeric(0) as Expr, ::add2)
+fun multiply(vararg args: Expr): Expr = args.asSequence().fold(LongNumeric(1) as Expr, ::multiply2)
+
+// def: (-) -> 0, (- 1) -> -1, (- 1 1) -> 0, (- 1 1 1) -> -1, ...
+// thus 0 and 1 arg are special cases, 2+ means that lfold seeded w/ first arg
+// see: https://www.gnu.org/software/mit-scheme/documentation/mit-scheme-ref/Numerical-operations.html
+fun subtract(vararg args: Expr): Expr = when {
+    args.isEmpty() -> LongNumeric(0)
+    args.size == 1 -> negate(args[0])
+    else -> args.asSequence().drop(1).fold(args[0], ::subtract2)
+}
+
+// def: (/) -> 1, (/ 2) -> 1/2, (/ 3 1) -> 3, (/ 30 5 3) -> 2, ...
+// thus 0 and 1 arg are special cases, 2+ means that lfold seeded w/ first arg
+// see: https://www.gnu.org/software/mit-scheme/documentation/mit-scheme-ref/Numerical-operations.html
+fun divide(vararg args: Expr): Expr = when {
+    args.isEmpty() -> LongNumeric(1)
+    args.size == 1 -> invert(args[0])
+    else -> args.asSequence().drop(1).fold(args[0], ::divide2)
+}
 
 
 fun Env.registerBuiltins() {
     update("+", Expr.Procedure(::add))
+    update("*", Expr.Procedure(::multiply))
+    update("-", Expr.Procedure(::subtract))
+    update("/", Expr.Procedure(::divide))
 }
--- src/test/BuiltinsTest.kt
+++ src/test/BuiltinsTest.kt
@@ -9,9 +9,6 @@ internal class BuiltinsTest {
         val env = Env(null, mutableMapOf())
         env.registerBuiltins()
 
-        val expr = "(+)".parseProgram()
-        val result = expr.evalIn(env)
-
         // basic longs and arities
         assertEquals(Expr.LongNumeric(0), "(+)".parseProgram().evalIn(env))
         assertEquals(Expr.LongNumeric(1), "(+ 1)".parseProgram().evalIn(env))
@@ -26,6 +23,97 @@ internal class BuiltinsTest {
         // confirm double casting
         assertEquals(Expr.DoubleNumeric(2.0), "(+ 1.0 1)".parseProgram().evalIn(env))
         assertEquals(Expr.DoubleNumeric(2.0), "(+ 1 1.0)".parseProgram().evalIn(env))
+
+        // oops
+        assertFails { "(+ a)".parseProgram().evalIn(env) }
+        assertFails { "(+ 1 a)".parseProgram().evalIn(env) }
+        assertFails { "(+ a 1)".parseProgram().evalIn(env) }
+        assertFails { "(+ 1 a 1)".parseProgram().evalIn(env) }
+        assertFails { "(+ 1 1 a)".parseProgram().evalIn(env) }
     }
 
+    @Test
+    fun test_multiply() {
+        val env = Env(null, mutableMapOf())
+        env.registerBuiltins()
+
+        // basic longs and arities
+        assertEquals(Expr.LongNumeric(1), "(*)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(1), "(* 1)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(3), "(* 3 1)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(6), "(* 1 2 3)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(3628800), "(* 1 2 3 4 5 6 7 8 9 10)".parseProgram().evalIn(env))
+
+        // basic doubles
+        assertEquals(Expr.DoubleNumeric(2.0), "(* 2.0)".parseProgram().evalIn(env))
+        assertEquals(Expr.DoubleNumeric(8.0), "(* 2.0 4.0)".parseProgram().evalIn(env))
+
+        // confirm double casting
+        assertEquals(Expr.DoubleNumeric(2.0), "(* 1.0 2)".parseProgram().evalIn(env))
+        assertEquals(Expr.DoubleNumeric(2.0), "(* 1 2.0)".parseProgram().evalIn(env))
+
+        // oops
+        assertFails { "(* a)".parseProgram().evalIn(env) }
+        assertFails { "(* 1 a)".parseProgram().evalIn(env) }
+        assertFails { "(* a 1)".parseProgram().evalIn(env) }
+        assertFails { "(* 1 a 1)".parseProgram().evalIn(env) }
+        assertFails { "(* 1 1 a)".parseProgram().evalIn(env) }
+    }
+
+
+    @Test
+    fun test_subtract() {
+        val env = Env(null, mutableMapOf())
+        env.registerBuiltins()
+
+        // basic longs and arities
+        assertEquals(Expr.LongNumeric(0), "(-)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(-1), "(- 1)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(0), "(- 1 1)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(-4), "(- 1 2 3)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(-53), "(- 1 2 3 4 5 6 7 8 9 10)".parseProgram().evalIn(env))
+
+        // basic doubles
+        assertEquals(Expr.DoubleNumeric(-1.0), "(- 1.0)".parseProgram().evalIn(env))
+        assertEquals(Expr.DoubleNumeric(0.0), "(- 1.0 1.0)".parseProgram().evalIn(env))
+
+        // confirm double casting
+        assertEquals(Expr.DoubleNumeric(0.0), "(- 1.0 1)".parseProgram().evalIn(env))
+        assertEquals(Expr.DoubleNumeric(0.0), "(- 1 1.0)".parseProgram().evalIn(env))
+
+        // oops
+        assertFails { "(- a)".parseProgram().evalIn(env) }
+        assertFails { "(- 1 a)".parseProgram().evalIn(env) }
+        assertFails { "(- a 1)".parseProgram().evalIn(env) }
+        assertFails { "(- 1 a 1)".parseProgram().evalIn(env) }
+        assertFails { "(- 1 1 a)".parseProgram().evalIn(env) }
+    }
+
+    @Test
+    fun test_divide() {
+        val env = Env(null, mutableMapOf())
+        env.registerBuiltins()
+
+        // basic longs and arities
+        assertEquals(Expr.LongNumeric(1), "(/)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(1), "(/ 1)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(3), "(/ 6 2)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(3), "(/ 12 2 2)".parseProgram().evalIn(env))
+        assertEquals(Expr.LongNumeric(0), "(/ 1 2 3 4 5 6 7 8 9 10)".parseProgram().evalIn(env))
+
+        // basic doubles
+        assertEquals(Expr.DoubleNumeric(1.0), "(/ 1.0)".parseProgram().evalIn(env))
+        assertEquals(Expr.DoubleNumeric(2.0), "(/ 1.0 0.5)".parseProgram().evalIn(env))
+
+        // confirm double casting
+        assertEquals(Expr.DoubleNumeric(0.5), "(/ 0.5 1)".parseProgram().evalIn(env))
+        assertEquals(Expr.DoubleNumeric(0.5), "(/ 1 2.0)".parseProgram().evalIn(env))
+
+        // oops
+        assertFails { "(/ a)".parseProgram().evalIn(env) }
+        assertFails { "(/ 1 a)".parseProgram().evalIn(env) }
+        assertFails { "(/ a 1)".parseProgram().evalIn(env) }
+        assertFails { "(/ 1 a 1)".parseProgram().evalIn(env) }
+        assertFails { "(/ 1 1 a)".parseProgram().evalIn(env) }
+    }
 }
